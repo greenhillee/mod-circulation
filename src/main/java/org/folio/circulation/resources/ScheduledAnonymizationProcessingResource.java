3049a899e6910684f9df1e25d774c03ae5a526a4
Marc Johnson <marc@k-int.co.uk>
2020-07-22 12:30:04 +0100

    Use safe initialisation to start loan anonymization process CIRC-850

--- src/main/java/org/folio/circulation/resources/ScheduledAnonymizationProcessingResource.java
+++ src/main/java/org/folio/circulation/resources/ScheduledAnonymizationProcessingResource.java
@@ -1,5 +1,7 @@
 package org.folio.circulation.resources;
 
+import static org.folio.circulation.support.results.AsynchronousResultBindings.safelyInitialise;
+
 import org.folio.circulation.domain.ConfigurationRepository;
 import org.folio.circulation.domain.anonymization.LoanAnonymization;
 import org.folio.circulation.domain.representations.anonymization.AnonymizeLoansRepresentation;
@@ -35,7 +37,7 @@ public class ScheduledAnonymizationProcessingResource extends Resource {
 
     LoanAnonymization loanAnonymization = new LoanAnonymization(clients);
 
-    configurationRepository.loanHistoryConfiguration()
+    safelyInitialise(configurationRepository::loanHistoryConfiguration)
       .thenCompose(r -> r.after(config -> loanAnonymization
           .byCurrentTenant(config).anonymizeLoans()))
       .thenApply(AnonymizeLoansRepresentation::from)
